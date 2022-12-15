5f0e3741b3a83c259979646d3f8dcc0f09292194
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-12-15 15:32:03 +0300

    add week 4 starter

---
+++ week4/starter.py
@@ -0,0 +1,9 @@
+carrots = int(input('How many carrots do you have? '))
+rabbits = 6
+
+if rabbits > carrots:
+    print('There are not enough carrots')
+elif rabbits < carrots:
+    print('There are too many carrots')
+else:
+    print('You have the right number of carrots')
