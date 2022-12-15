ae127f9468d0d27e8eaea09111f44123c83d3e49
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-12-15 15:43:50 +0300

    add week 4 homework question 1

---
+++ week4/Homework/Q1.py
@@ -0,0 +1,18 @@
+"""
+Question 1
+I have a list of things I need to buy from my supermarket of choice.
+I want to know what the first thing I need to buy is.
+However, when I run the program it shows me a
+different answer to what I was expecting?
+What is the mistake? How do I fix it?
+"""
+shopping_list = [
+    "oranges",
+    "cat food",
+    "sponge cake",
+    "long-grain rice",
+    "cheese board",
+]
+
+print(shopping_list[0])
+# first item is always index 0
