9def5a8837929ec21387cc6e46e3c46cb0a6c9d6
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-01-05 23:42:21 +0300

    add week 3 homework

---
+++ week6/mycsv.py
@@ -0,0 +1,13 @@
+import csv
+
+# Add code to open the csv file
+with open('trees.csv', 'r') as csv_file:
+    spreadsheet = csv.DictReader(csv_file)
+    heights = []
+
+    for row in spreadsheet:
+        tree_height = row['height']
+        heights.append(tree_height)
+
+shortest_height = min(heights)
+print(shortest_height)
