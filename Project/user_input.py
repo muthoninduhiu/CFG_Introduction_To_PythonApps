54d4aebc2814c5b7c7efb1c81558223648abe0e8
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-01-26 10:44:47 +0300

    add a way to order items

--- Project/user_input.py
+++ Project/user_input.py
@@ -1,5 +1,8 @@
 from recipe_search import recipe_search
 
+print('Welcome to Recipe Search!\n'
+      'We\'ll choose a couple of recipes based on an ingredient of your choice')
+
 # Ask the user to enter an ingredient that they want to search for, ask extra question
 has_diet = input('Are you On A Diet?? ')
 if has_diet == 'yes':
@@ -39,7 +42,7 @@ def user_input():
         Function that asks for user  input and uses that to search for the value in the API
         then save the results to a file
         """
-    list_of_recipes = recipe_search(ingredient, diet_restriction, meal_type)
+    list_of_recipes = recipe_search(ingredient, has_meal_type, has_diet)
 
     if not list_of_recipes:
         print("No recipe matches your search!")
@@ -52,10 +55,26 @@ def user_input():
                 recipe_details.write("Name: {}\nThe Ingredients: {}\nWeight: {}\nCuisine Type: {}\nHealth Label: {}\n"
                                      .format(recipes['label'],
                                              (recipes['ingredientLines']),
-                                             (recipes['totalWeight']),
+                                             (recipes['mealType']),
                                              recipes['cuisineType'],
                                              recipes['healthLabels']))
                 recipe_details.write("\n")
+            # order results for some parameter : totalWeight,calories,total_time,yield
+            recipe_weight = []
+            for weight in list_of_recipes:
+                recipe_weight.append(weight['recipe']['totalWeight'])
+                # print(recipe_weight)
+                print(sorted(recipe_weight))
+
+            recipe_calories = []
+            for calories in list_of_recipes:
+                recipe_calories.append(calories['recipe']['calories'])
+                print(sorted(recipe_calories))
+
+            recipe_total_time = []
+            for time in list_of_recipes:
+                recipe_total_time.append(time['recipe']['totalTime'])
+                print(sorted(recipe_total_time))
 
 
 # Display the recipes for each search result
@@ -64,6 +83,9 @@ def read_text():
             :Purpose: Read data in the text file and display it
             :return: has no return type
             """
-    with open('recipe_names.txt', 'r') as text_file:
+    with open('recipe_names.txt', 'r', encoding='utf-8') as text_file:
         contents = text_file.read()
         print(contents)
+
+
+user_input()
